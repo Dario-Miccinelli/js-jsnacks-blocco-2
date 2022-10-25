92e54d773fef925b0de89254f4b02e01856b7e90
Dario-Miccinelli <poduzzo@hotmail.com>
2022-10-25 19:13:24 +0200

    terzo snack

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,10 @@
+alert ('Tutti gli esercizi sono onclick con output in console')
+
+
+
+
 // Primo Snack
 //Inserisci un numero, se è pari stampa il numero, se è dispari stampa il numero successivo ( del numero inserito dall'utente: esempio se il numero dell'utente è 2, il numero successivo è 3  ).
-
-
-
 
 function snippet1(){
     let num = parseInt(prompt('Numero?'));
@@ -31,5 +33,32 @@ let nlastname = lastname.length;
 
 
 console.log(firstname[Math.round(Math.random() * (nfirstname - 1))] + ' ' + lastname[Math.round(Math.random() * (nlastname -1 ))])
+
+}
+
+
+//Terzo Snack
+// Crea un array di numeri interi e fai la somma di tutti gli elementi  che sono in posizione (indice) dispari
+//I numeri sommati saranno 1+3+4 in quanto l'array parte dalla posizione 0
+
+function snippet3() {
+    let numbers = [1 , 1 , 5, 3, 9, 4];
+
+let sum = 0;
+
+for(let i = 1; i < numbers.length; i= i+2){
+    sum += numbers[i];
+}
+
+console.log("La somma equivale a: ", sum)
+
+}
+
+//Quarto Snack (bonus)
+//Crea due array che hanno un numero di elementi diversi.  Aggiungi elementi all’array che ha meno elementi fino a quando ne avrà tanti quanti l’altro.
+
+function snippet4() {
+alert ('Non riesco :(')
+    
 
 }
